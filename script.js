1df7ce3c65b523e65a55848bd5d4053bfb4faa80
tusharnankani <tusharnankani3@gmail.com>
2022-11-16 00:47:09 +0530

    :sparkles: feat: create on spacebar keydown

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let body = document.getElementsByTagName('body')[0];
 
-body.addEventListener('click', (event) => {
+createNewCircle = () => {
     let newCircle = document.createElement('div');
     newCircle.classList.add('circle');
     
     body.appendChild(newCircle);
     setTimeout(() => body.removeChild(newCircle), 4000);
-});
+}
+
+body.addEventListener('click', (event) => {
+    createNewCircle();
+})
+
+body.addEventListener("keydown", (event) => {
+    if(event.key === " ")
+        createNewCircle(); 
+})
